4ff33e98214e5f3529895eb7a16bd2c95fd4d2d5
Guy Twig <twigy77@gmail.com>
2024-08-07 11:57:06 +0300

    remove main.storyboard and make CountriesListVC rootVC of project
        func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
            // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
            // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
            // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
            guard let windowScene = (scene as? UIWindowScene) else {
                return
            }
    
            let window = UIWindow(windowScene: windowScene)
            let initialViewController = CountriesListVC(nibName: "CountriesListVC", bundle: nil)
            let navigationController = UINavigationController(rootViewController: initialViewController)
            window.rootViewController = navigationController
            self.window = window
            window.makeKeyAndVisible()

---
+++ CountriesRx/CountriesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  CountriesListVC.swift
+//  CountriesRx
+//
+//  Created by Guy Twig on 07/08/2024.
+//
+
+import UIKit
+
+class CountriesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
