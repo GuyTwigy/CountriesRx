cdf7204992e1642de3d0867078b471d627d9f8f7
Guy Twig <twigy77@gmail.com>
2024-08-11 01:54:46 +0300

    add 4 calls running together

--- CountriesRx/VCs/CountriesList/vm/CountriesListVM.swift
+++ CountriesRx/VCs/CountriesList/vm/CountriesListVM.swift
@@ -24,30 +24,31 @@ class CountriesListVM {
     var dataService = NetworkManager()
     private let disposeBag = DisposeBag()
     
-    init() {
-        Task {
-            await fetchCountries()
-        }
-    }
-    
+    init() {}
     func fetchCountries() async {
         do {
             let realmCountries = try await RealmManager.shared.fetchRealmCountries()
             savedList = realmCountries
             countryModifiedList.removeAll()
             countryModifiedList = savedList
-            
-            dataService.fetchCountries()
+
+            // Initiate four independent fetches that run concurrently
+            let fetch1 = dataService.fetchCountries().asObservable()
+            let fetch2 = dataService.fetchCountries().asObservable()
+            let fetch3 = dataService.fetchCountries().asObservable()
+            let fetch4 = dataService.fetchCountries().asObservable()
+
+            Observable.zip(fetch1, fetch2, fetch3, fetch4)
                 .observe(on: MainScheduler.instance)
-                .subscribe(onSuccess: { [weak self] countries in
-                    guard let self else {
-                        self?.countryList.onNext(self?.countryModifiedList ?? [])
-                        self?.countryList.onCompleted()
-                        self?.delegate?.countriesFetched(error: ErrorsHandlers.requestError(.decodingError("Fail when try to subscribe")))
-                        return
-                    }
+                .subscribe(onNext: { [weak self] result1, result2, result3, result4 in
+                    guard let self else { return }
                     
-                    let filteredCountryList = countries.filter { country in
+                    print("result1: num of countries \(result1.count)")
+                    print("result2: num of countries \(result2.count)")
+                    print("result3: num of countries \(result3.count)")
+                    print("result4: num of countries \(result4.count)")
+                    
+                    let filteredCountryList = result1.filter { country in
                         !self.countryModifiedList.contains { $0.name?.common == country.name?.common }
                     }
                     
@@ -55,7 +56,7 @@ class CountriesListVM {
                     self.countryList.onNext(self.countryModifiedList)
                     self.notFilteredCountryList = self.countryModifiedList
                     self.delegate?.countriesFetched(error: nil)
-                }, onFailure: { [weak self] error in
+                }, onError: { [weak self] error in
                     guard let self else {
                         self?.countryList.onNext(self?.countryModifiedList ?? [])
                         self?.countryList.onCompleted()
@@ -63,12 +64,13 @@ class CountriesListVM {
                         return
                     }
                     
-                    self.countryList.onNext(countryModifiedList)
+                    self.countryList.onNext(self.countryModifiedList)
                     self.delegate?.countriesFetched(error: error)
                 })
                 .disposed(by: disposeBag)
         } catch {
-            print("Fail to fetch fav list, error: \(error.localizedDescription)")
+            print("Failed to fetch fav list, error: \(error.localizedDescription)")
+            delegate?.countriesFetched(error: error)
         }
     }
     
